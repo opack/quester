9adff27a8937113afa85217d0332ef4600185f3e
Opack <opack@users.noreply.github.com>
2013-06-05 15:03:15 +0000

    IN PROGRESS : Suppression du contextpad et ajout d'un bouton pour arrêter le déplacement en cours

---
+++ quester-core/src/main/java/com/slamdunk/quester/display/hud/contextpad/StopActionOnClickListener.java
@@ -0,0 +1,16 @@
+package com.slamdunk.quester.display.hud.contextpad;
+
+import com.slamdunk.quester.core.QuesterGame;
+import com.slamdunk.quester.display.actors.Player;
+
+public class StopActionOnClickListener extends ActionOnClickListener {
+	
+	public StopActionOnClickListener(Player player) {
+		super(player, 0, 0);
+	}
+
+	@Override
+	public void onClick() {
+		player.
+	}
+}
